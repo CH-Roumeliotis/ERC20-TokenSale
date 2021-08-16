8c6a4a3b25400cefb7eac36d5a3c4a88e5518f2e
CH-Roumeliotis <christosroumeliotisfg@gmail.com>
2021-08-16 18:52:25 +0300

    configuration

--- truffle-config.js
+++ truffle-config.js
@@ -22,7 +22,7 @@
 // const infuraKey = "fj4jll3k.....";
 //
 // const fs = require('fs');
-require('dotenv').config();
+//require('dotenv').config();
 
 module.exports = {
   /**
@@ -58,14 +58,14 @@ module.exports = {
     // },
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
-     ropsten: {
-     provider: () => new HDWalletProvider(process.env.MNEMONIC, `https://ropsten.infura.io/v3/`+ process.env.INFURA_API_KEY),
-     network_id: 3,       // Ropsten's id
-     gas: 5500000,        // Ropsten has a lower block limit than mainnet
+     //ropsten: {
+     //provider: () => new HDWalletProvider(process.env.MNEMONIC, `https://ropsten.infura.io/v3/`+ process.env.INFURA_API_KEY),
+     //network_id: 3,       // Ropsten's id
+     //gas: 5500000,        // Ropsten has a lower block limit than mainnet
     // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
     // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
     // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-     },
+     //},
     // Useful for private networks
     // private: {
     // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
