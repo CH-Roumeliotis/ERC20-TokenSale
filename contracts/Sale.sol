a9605e290974e9cb471413fa44acc74d49374655
CH-Roumeliotis <christosroumeliotisfg@gmail.com>
2021-08-16 18:24:36 +0300

    erc20

---
+++ contracts/Sale.sol
@@ -0,0 +1,48 @@
+pragma solidity ^0.5.0;
+
+import "./ERC20basic.sol";
+
+contract tokenSale {
+    address payable owner;
+    CHRToken public tokenContract;
+    uint256 public tokenPrice;
+    uint256 public tokensSold;
+
+    event sell(address _buyer, uint256 _amount);
+
+    constructor(CHRToken _tokenContract, uint256 _tokenPrice) public {
+        owner = msg.sender;
+        tokenContract = _tokenContract;
+        tokenPrice = _tokenPrice;
+    }
+
+    function buyTokens(uint256 _numberOfTokens) public payable {
+        require(
+            msg.value == _numberOfTokens * tokenPrice,
+            "Number of tokens does not match with the value"
+        );
+        require(
+            tokenContract.balanceOf(address(this)) >= _numberOfTokens,
+            "Contact does not have enough tokens"
+        );
+        require(
+            tokenContract.transfer(msg.sender, _numberOfTokens),
+            "Some problem with token transfer"
+        );
+        tokensSold += _numberOfTokens;
+        emit sell(msg.sender, _numberOfTokens);
+    }
+
+    function endSale() public {
+        require(msg.sender == owner, "Only the owner");
+        require(
+            tokenContract.transfer(
+                msg.sender,
+                tokenContract.balanceOf(address(this))
+            ),
+            "Unable to transfer tokens"
+        );
+        // destroy contract
+        selfdestruct(owner);
+    }
+}
